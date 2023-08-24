bbc2ee3a07daffb6b3b39b6203b0e6dedc0cda9c
Greg Cain <codegregcode@gmail.com>
2023-08-24 14:19:03 +0100

    sets up project

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('renders Hello World', () => {
+  render(<App />);
+  const hwElement = screen.getByText(/Hello World/i);
+  expect(hwElement).toBeInTheDocument();
+});
