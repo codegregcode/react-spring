d879b7502b91ca766afb1e0f33ab13b30691e09a
Greg Cain <codegregcode@gmail.com>
2023-08-24 14:49:51 +0100

    adds first react spring animation :):):)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
 import '../styles/app.css';
+import { useSpring, animated } from '@react-spring/web';
 
 function App() {
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0 },
+  }));
+
+  const handleClick = () => {
+    api.start({
+      from: { x: 0 },
+      to: { x: 1500 },
+    });
+  };
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <animated.div
+        onClick={handleClick}
+        style={{
+          width: 80,
+          height: 80,
+          background: '#ff6d6d',
+          borderRadius: 8,
+          ...springs,
+        }}
+      />
     </div>
   );
 }
