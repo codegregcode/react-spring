3430860ca4ffcadf37d42b664a0225ad4b8d18a1
Greg Cain <codegregcode@gmail.com>
2023-09-05 18:30:04 +0100

    rewrites App tests and creates snapshot

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,22 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../components/App';
 
 describe('App', () => {
-  it('renders Hello World', () => {
-    render(<App />);
-    const hwElement = screen.getByText(/Hello World/i);
-    expect(hwElement).toBeInTheDocument();
-  });
-
-  it('renders the animated sqr', () => {
-    render(<App />);
-    const aniSqr = screen.getByTestId('animated-sqr');
-    expect(aniSqr).toBeInTheDocument();
-  });
-
-  it('renders the 2nd animated sqr', () => {
-    render(<App />);
-    const sqr2 = screen.getByTestId('animated-sqr2');
-    expect(sqr2).toBeInTheDocument();
+  it('renders App correctly', () => {
+    const { appFragment } = render(<App />);
+    expect(appFragment).toMatchSnapshot();
   });
 });
